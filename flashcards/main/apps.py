86331b74d27bf3ac22af6fb857775156ca6a9b3f
Koltasaurusrex <koltonheaps@gmail.com>
2021-06-04 16:14:00 -0600

    CHanged app name again, framework for custom templates and static files, basic flash card view

--- flashcards/main/apps.py
+++ flashcards/main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FlashcardsappConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flashcardsapp'
+    name = 'main'
