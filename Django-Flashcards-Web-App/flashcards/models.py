2475d3a0e40c72d44eeb96609d1e7b0dfd153ea8
Koltasaurusrex <koltonheaps@gmail.com>
2021-06-17 22:58:12 -0600

    Create card always at end

--- Django-Flashcards-Web-App/flashcards/models.py
+++ Django-Flashcards-Web-App/flashcards/models.py
@@ -6,11 +6,11 @@ class Card_Set(models.Model):
     '''
     Card_Set model functions as the deck that a card belongs to
     A deck has a topic and description
-    '''                                                        
+    '''
     topic = models.CharField(max_length = 50, null = False, blank = False)       # Topic of the Deck
     description = models.CharField(max_length = 300, null = False, blank = True) # Description of topic
-    created_by = models.CharField(max_length= 300, null = False, blank = True)
-    is_active = models.BooleanField(default = True)   # When Card_Set is created, it will default to 
+    created_by = models.CharField(max_length= 300, null = False, blank = True) # Show who the card set was created by
+    is_active = models.BooleanField(default = True)   # When Card_Set is created, it will default to
                                                       # being inactive as no cards will exist in the set
     # __str__ is a function that modifies string representation
     # When Cards.objects.all() is run, the name of the word will be displayed
@@ -23,13 +23,14 @@ class Card_Set(models.Model):
         '''
         Returns the card count in each deck as an integer
         '''
+        # get_card_count.short_description = 'Card Count'  # Card Count is the column header in Admin Panel
         return self.card_set.count()
-    get_card_count.short_description = 'Card Count'  # Card Count is the column header in Admin Panel
+
 
 
     def get_random_card(self):
         '''
-        Returns a random card from the deck 
+        Returns a random card from the deck
         '''
         random_number = random.randint(0, self.card_set.count() - 1)    # Return random card from deck
         random_card = self.card_set.all()[random_number]
@@ -38,16 +39,16 @@ class Card_Set(models.Model):
 
 class Card(models.Model):
     '''
-    Card model for the cards in the deck 
+    Card model for the cards in the deck
     Parent is the Card_Set model
     Each card has word, definition, and sentence
     '''
     parent_card_set = models.ForeignKey(Card_Set, on_delete = models.CASCADE)
-    
+
     word = models.CharField(max_length = 50, null = False, blank = False)         # Vocabulary word
     definition = models.TextField(max_length = 500, null = False, blank = False)  # Word's definition
     sentences = models.TextField(max_length = 500, null = False, blank = True)    # Word used in sentence
-    
+
 
     def __str__(self):
         return self.word
@@ -96,4 +97,3 @@ class Card(models.Model):
             return self.parent_card_set.card_set.first()
         else:
             return self.parent_card_set.card_set.filter(id__gt = self.id).first()
-
